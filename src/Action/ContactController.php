845dd0dd3ea838cfcd91fc77c34c5b1984fec389
Benjamin H Boruff <11185240+benjaminboruff@users.noreply.github.com>
2024-03-08 15:47:15 -0500

    basic old-school form

--- src/Action/ContactController.php
+++ src/Action/ContactController.php
@@ -13,17 +13,26 @@ use Slim\Views\Twig;
 class ContactController
 {
     private ContactRepository $contactRepository;
+    // private array|null $contacts = null;
 
     public function __construct(ContainerInterface $c)
     {
         $this->contactRepository = $c->get(EntityManager::class)->getRepository('App\Domain\Contact');
     }
 
-    public function index(Request $request, Response $response): Response
+    public function index(Request $request, Response $response, array $args = null): Response
     {
-        $contacts = $this->contactRepository->getAllContacts();
+        $search = $request->getQueryParams()['q'] ?? null;
+        $contacts = null;
+
+        if ($search) {
+            $contacts = $this->contactRepository->searchContacts($search);
+        } else {
+            $contacts = $this->contactRepository->getAllContacts();
+        }
+
         $view = Twig::fromRequest($request);
 
-        return $view->render($response, 'contacts.twig', ['contacts' => $contacts]);
+        return $view->render($response, 'contacts.twig', ['contacts' => $contacts, 'search' => $search]);
     }
 }
--- src/Repository/ContactRepository.php
+++ src/Repository/ContactRepository.php
@@ -17,4 +17,20 @@ final class ContactRepository extends EntityRepository
         $query->setMaxResults($number);
         return $query->getResult();
     }
+
+    public function searchContacts(string $search, $number = 30): mixed
+    {
+
+        $dql = <<<QUERY
+        SELECT u FROM App\Domain\Contact u
+        where u.firstName = '$search' 
+            OR u.lastName = '$search'
+            OR u.firstName LIKE '%$search%'
+            OR u.lastName LIKE '%$search%'
+        QUERY;
+
+        $query = $this->getEntityManager()->createQuery($dql);
+        $query->setMaxResults($number);
+        return $query->getResult();
+    }
 }
